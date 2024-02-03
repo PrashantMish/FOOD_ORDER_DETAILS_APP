86353877d0c5e00b5401b36385adaf2f4dc6928c
Prashant Mishra <69843175+PrashantMish@users.noreply.github.com>
2024-02-03 14:58:28 +0530

    FOOD_ORDER_DETAILS_APP

---
+++ food_order.py
@@ -0,0 +1,36 @@
+
+# In this i have written code for post request
+################################# Here, I am importing "json" and "requests" ############################
+
+import json
+import requests
+
+################################# Here, I have Written URL ############################
+
+base_url = "https://canteen.benzyinfotech.com/api/v3/customer/report"
+
+################################# Here, I have Written Token ############################
+
+auth_token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZWRhNWExODU0OTFhYWE0MmY5YzMyZjRhMTU5MDM1ODk4ZjZiMzMxNWUzZjJjNGRiZDA1N2IyNGE3NTAzMDc3NDBlMjFlYjZmNGE4Mjk0MGUiLCJpYXQiOjE3MDQ4MDA4OTAuODc5OTI1OTY2MjYyODE3MzgyODEyNSwibmJmIjoxNzA0ODAwODkwLjg3OTkyOTA2NTcwNDM0NTcwMzEyNSwiZXhwIjoxNzM2NDIzMjkwLjgzNDkxMjA2MTY5MTI4NDE3OTY4NzUsInN1YiI6IjI2NSIsInNjb3BlcyI6W119.CwDEjlHoRtOXdFcaO6KGGxV202AOA7MMtJVPtKzgLqzTFzUUnDLGBd7PNAtHO2--3YOathM9HOG8hYjY8wjktXZIoCGUR9GWIaEVUxLwFq927CrSf05NuqTBTrJcDeBOjXDvKcSBiJ2A994FC2IunPcdkaZ4jpoaWBIaWueYUbHviYSQuLec3tFcAMg4njrImAlaN9k-QKkHetpdrdbUEX1Wzq4X-1QwuOx7W3W2nbbxaoNgFX1gaabxi00ZO7h5MokGvtqy_gCkS9TYoM74VfxmTyAAczjttLcPqDNiAL_ZJdutDMezw32CZj8G8l8PUL46F_BuaxatZDBUZxeClZh4_0Wvo9GX4zqF2XvHdzZHnwdB414vNCl8itaGW9w7QWbdchPOglhnek32ZmkH0MIqeOBhnAyHo5_WbP0uLd_3qmz3w04nvTbTGV25-QebaxPAsVD0-7Za1sVpqB_FD6yEeliaEzdxl_8gA5IH59uowpfPYgUIjom8NVEASuYsAwb0q3f0jhNRfwg2zmXNenoDunh_dN9l2NRjI2gdZueSMwu6IJLQK46jpn01uG2iQ1xx-pFJAGe_bzSceLsho3dbtabym3tMqi0Ac02xUP9Mn50LdkFJGNVU9jiuHQfyjQirDtGUfya3aIvpJlCGx9Cx99s_4P89uDnOiXy3A1Q"
+
+################################# Here, DEFINING POST RREQUEST ############################
+
+def post_request():
+
+    url = base_url
+    headers = {"Authorization": auth_token}
+    Params = {"month": "11"} 
+    response = requests.post(url, headers=headers, params=Params)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        json_str = json.dumps(json_data, indent=4)
+        print("json_response_method =", json_str)
+    else:
+        print("Show_Error:", response.status_code)
+        print("Show_Response:", response.text)
+    
+
+################################# Here, Calling POST RREQUEST ############################
+        
+post_request()
